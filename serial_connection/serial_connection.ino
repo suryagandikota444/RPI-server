758c5fc57bf2ea3624042b5dea45a0a98e8616b7
Surya Gandikota <sgandik@gmail.com>
2023-03-09 19:03:16 +0000

    please work test 1

--- serial_connection/serial_connection.ino
+++ serial_connection/serial_connection.ino
@@ -3,14 +3,7 @@
 #include <pixeltypes.h>
 
 //Defining the LED pins
-#define LED_pin 10
-#define NUM_LEDs 21
-
-#include "FastLED.h"
-#include <pixeltypes.h>
-
-//Defining the LED pins
-#define LED_pin 10
+#define LED_pin 12
 #define NUM_LEDs 21
 
 const int stepPin = 3;
@@ -22,10 +15,7 @@ int numOfPulses = 0;
 
 CRGB LedStrip[NUM_LEDs];
 
-
-
-void setup() 
-{
+void setup() {
   // Sets the two pins as Outputs
   pinMode(stepPin, OUTPUT);
   pinMode(dirPin, OUTPUT);
@@ -33,158 +23,430 @@ void setup()
   Serial.setTimeout(1);
   FastLED.addLeds<WS2812B, LED_pin, GRB>(LedStrip,NUM_LEDs);
 }
-void loop() 
-{
-  while (Serial.available()) 
-  {
+void loop() {
+  while (Serial.available()) {
     String input = Serial.readString();                                            //getting the input - "A1" or "A2" or "C7" or "B3" 
     char targetLocation = input.charAt(0);                         // Parsing the input for theletter - 'A' or 'B' or 'C' 
     int targetIndex = String(input.charAt(1)).toInt();  // Parsing theinput for the number - 0, 1, 2, 3, 4, 5, 6, 7
 
-  if((currentLocation == 'A' || currentLocation == 'B') && (targetLocation == 'A' || targetLocation == 'B'))
+if((currentLocation == 'A' || currentLocation == 'B') && (targetLocation == 'A' || targetLocation == 'B'))
+{
+  int temp = (targetIndex - currentIndex) * 50;
+  if (temp < 0) 
   {
-    int temp = (targetIndex - currentIndex) * 50;
-    if (temp < 0) 
-    {
-      int temp2 = temp + 200;
-      numOfPulses = temp2 * 3;
-    } else 
-    {
-      numOfPulses = temp * 3;
-    }
-
-    Serial.println("(Before) Location: " + String(currentLocation) + "" + String(currentIndex));
-    Serial.println("(After) Location: " + input);
-    Serial.println("Steps taken: " + String(numOfPulses));
-    Serial.println();
-  }
-  else if ((currentLocation == 'A' || currentLocation == 'B') && (targetLocation == 'C')) {
-    int positionC = targetIndex / 2;
-    int temp = (positionC - currentIndex) * 50;
-    if (temp < 0) 
-    {
-      int temp2 = temp + 200;
-      numOfPulses = temp2 * 3;
-    } else 
-    {
-      numOfPulses = temp * 3;
-    }
-
-    Serial.println("(Before) Location: " + String(currentLocation) + "" + String(currentIndex));
-    Serial.println("(After) Location: " + input);
-    Serial.println("Steps taken: " + String(numOfPulses));
-    Serial.println();
-  }
-  else if ((currentLocation == 'C') && (targetLocation == 'A' || targetLocation == 'B')) 
+    int temp2 = temp + 200;
+    numOfPulses = temp2 * 3;
+  } else 
   {
-    int positionC = currentIndex / 2;
-    int temp = (targetIndex - positionC) * 50;
-    if (temp < 0) 
-    {
-      int temp2 = temp + 200;
-      numOfPulses = temp2 * 3;
-    } else 
-    {
-      numOfPulses = temp * 3;
-    }
-
-    if(targetLocation == 'A') 
-    {
-          //First Set of LEDS light up
-          for(int LEDIt = 0; LEDIt<21; LEDIt++)
-          {
-            if(LEDIt<=7)
-            {
-              LedStrip[LEDIt]=CRGB::DarkOrchid;
-            }
-            else
-            {
-              LedStrip[LEDIt]=CRGB::Black;
-            }
-            
-          }
-          FastLED.show();    
-          delay(10);  
-        }
-        if(targetLocation == 'B') 
-        {
-          //First Set of LEDS light up
-          for(int LEDIt = 0; LEDIt<21; LEDIt++)
-          {
-            if(LEDIt>7 && LEDIt<14)
-            {
-              LedStrip[LEDIt]=CRGB::DarkOrchid;
-            }
-            else
-            {
-              LedStrip[LEDIt]=CRGB::Black;
-            }
-          }
-          FastLED.show();     
-          delay(10); 
-        }   
-        
-    Serial.println("(Before) Location: " + String(currentLocation) + "" + String(currentIndex));
-    Serial.println("(After) Location: " + input);
-    Serial.println("Steps taken: " + String(numOfPulses));
-    Serial.println();
+    numOfPulses = temp * 3;
   }
-  else if (currentLocation == 'C' && targetLocation == 'C') 
+
+
+  if(targetLocation == 'A') 
   {
-    int positionC = targetIndex / 2;
-    currentIndex = currentIndex / 2;
-    int temp = (positionC - currentIndex) * 50;
-
-    if (temp < 0) 
+    //First Set of LEDS light up
+    for(int LEDIt = 0; LEDIt<20; LEDIt++)
     {
-      int temp2 = temp + 200;
-      numOfPulses = temp2 * 3;
-    } else 
-    {
-      numOfPulses = temp * 3;
-    }
-
-    Serial.println("(Before) Location: " + String(currentLocation) + "" + String(currentIndex));
-    Serial.println("(After) Location: " + input);
-    Serial.println("Steps taken: " + String(numOfPulses));
-    Serial.println();
-
-    if(targetLocation == 'C') 
-    {
-          //First Set of LEDS light up
-          
-            for(int LEDIt = 0; LEDIt<21; LEDIt++)
-            {
-            if(LEDIt>14)
-            {
-              LedStrip[LEDIt]=CRGB::DarkOrchid;
-            }
-
-            else
-            {
-              LedStrip[LEDIt]=CRGB::Black;
-            }
-          }
+      if(LEDIt<7)
+      {
+        LedStrip[LEDIt] = 0x009999;
       }
-          FastLED.show();     
-          delay(10); 
-        
-
+      else
+      {
+        LedStrip[LEDIt]= CRGB::Black;
+      }   
+    }
+    FastLED.show();    
+    delay(10);  
+  }
+  else if(targetLocation == 'B') 
+  {
+    //First Set of LEDS light up
+    for(int LEDIt = 0; LEDIt<21; LEDIt++)
+    {
+      if(LEDIt>7 && LEDIt<14)
+      {
+        LedStrip[LEDIt] = 0x009999;
+      }
+      else
+      {
+        LedStrip[LEDIt]=CRGB::Black;
+      }
+    }
+    FastLED.show();    
+    delay(10);  
   }
 
-  //Signal the motor now
-    digitalWrite(dirPin, HIGH);
-    for (int y = 0; y < (numOfPulses); y++) 
+  Serial.println("(Before) Location: " + String(currentLocation) + "" + String(currentIndex));
+  Serial.println("(After) Location: " + input);
+  Serial.println("Steps taken: " + String(numOfPulses));
+  Serial.println();
+}
+else if ((currentLocation == 'A' || currentLocation == 'B') && (targetLocation == 'C')) {
+  int positionC = targetIndex / 2;
+  int temp = (positionC - currentIndex) * 50;
+  if (temp < 0) 
+  {
+    int temp2 = temp + 200;
+    numOfPulses = temp2 * 3;
+  } 
+  else 
+  {
+    numOfPulses = temp * 3;
+  }
+
+  if(targetLocation == 'C') 
+  {
+    //First Set of LEDS light up    
+    for(int LEDIt = 0; LEDIt<21; LEDIt++)
     {
-      digitalWrite(stepPin, HIGH);
-      delayMicroseconds(3000);
-      digitalWrite(stepPin, LOW);
-      delayMicroseconds(3000);
+      if(LEDIt>14)
+      {
+        LedStrip[LEDIt] = 0x009999;
+      }
+      else
+      {
+        LedStrip[LEDIt]=CRGB::Black;
+      }
     }
-    numOfPulses = 0;
-    delay(1000);                       //One second delay
-    currentLocation = targetLocation;  //Current position is now the old target position
-    currentIndex = targetIndex;
-    
+  
+    FastLED.show();     
+    delay(10);
+  }
+  Serial.println("(Before) Location: " + String(currentLocation) + "" + String(currentIndex));
+  Serial.println("(After) Location: " + input);
+  Serial.println("Steps taken: " + String(numOfPulses));
+  Serial.println();
+}
+else if ((currentLocation == 'C') && (targetLocation == 'A' || targetLocation == 'B')) {
+  int positionC = currentIndex / 2;
+  int temp = (targetIndex - positionC) * 50;
+  if (temp < 0) 
+  {
+    int temp2 = temp + 200;
+    numOfPulses = temp2 * 3;
+  } 
+  else 
+  {
+    numOfPulses = temp * 3;
+  }
+
+  if(targetLocation == 'A') 
+  {
+    //First Set of LEDS light up
+    for(int LEDIt = 0; LEDIt<21; LEDIt++)
+    {
+      if(LEDIt<=7)
+      {
+        LedStrip[LEDIt] = 0x009999;
+      }
+      else
+      {
+        LedStrip[LEDIt]=CRGB::Black;
+      }   
+    }
+    FastLED.show();    
+    delay(10);  
+  }
+  else if(targetLocation == 'B') 
+  {
+    //First Set of LEDS light up
+    for(int LEDIt = 0; LEDIt<21; LEDIt++)
+    {
+      if(LEDIt>7 && LEDIt<14)
+      {
+        LedStrip[LEDIt] = 0x009999;
+      }
+      else
+      {
+        LedStrip[LEDIt]=CRGB::Black;
+      }
+    }
+    FastLED.show();    
+    delay(10);  
+  }
+       
+  Serial.println("(Before) Location: " + String(currentLocation) + "" + String(currentIndex));
+  Serial.println("(After) Location: " + input);
+  Serial.println("Steps taken: " + String(numOfPulses));
+  Serial.println();
+}
+else if (currentLocation == 'C' && targetLocation == 'C') 
+{
+  int positionC = targetIndex / 2;
+  currentIndex = currentIndex / 2;
+  int temp = (positionC - currentIndex) * 50;
+
+  if (temp < 0) 
+  {
+    int temp2 = temp + 200;
+    numOfPulses = temp2 * 3;
+  } else 
+  {
+    numOfPulses = temp * 3;
+  }
+  if(targetLocation == 'C') 
+  {
+    //First Set of LEDS light up    
+    for(int LEDIt = 0; LEDIt<21; LEDIt++)
+    {
+      if(LEDIt>14)
+      {
+        LedStrip[LEDIt]= 0x009999;
+      }
+      else
+      {
+        LedStrip[LEDIt]=CRGB::Black;
+      }
+    }
+    FastLED.show();     
+    delay(10);
+  }
+  Serial.println("(Before) Location: " + String(currentLocation) + "" + String(currentIndex));
+  Serial.println("(After) Location: " + input);
+  Serial.println("Steps taken: " + String(numOfPulses));
+  Serial.println();
+
+}
+
+//Signal the motor now
+digitalWrite(dirPin, HIGH);
+for (int y = 0; y < (numOfPulses); y++) 
+{
+  digitalWrite(stepPin, HIGH);
+  delayMicroseconds(3000);
+  digitalWrite(stepPin, LOW);
+  delayMicroseconds(3000);
+}
+numOfPulses = 0;
+delay(1000);                       //One second delay
+currentLocation = targetLocation;  //Current position is now the old target position
+  currentIndex = targetIndex;
+  }
+}
+// Defines pins numbers
+#include "FastLED.h"
+#include <pixeltypes.h>
+
+//Defining the LED pins
+#define LED_pin 12
+#define NUM_LEDs 21
+
+const int stepPin = 3;
+const int dirPin = 4;
+
+char currentLocation = 'A';
+int currentIndex = 0;
+int numOfPulses = 0;
+
+CRGB LedStrip[NUM_LEDs];
+
+void setup() {
+  // Sets the two pins as Outputs
+  pinMode(stepPin, OUTPUT);
+  pinMode(dirPin, OUTPUT);
+  Serial.begin(115200);
+  Serial.setTimeout(1);
+  FastLED.addLeds<WS2812B, LED_pin, GRB>(LedStrip,NUM_LEDs);
+}
+void loop() {
+  while (Serial.available()) {
+    String input = Serial.readString();                                            //getting the input - "A1" or "A2" or "C7" or "B3" 
+    char targetLocation = input.charAt(0);                         // Parsing the input for theletter - 'A' or 'B' or 'C' 
+    int targetIndex = String(input.charAt(1)).toInt();  // Parsing theinput for the number - 0, 1, 2, 3, 4, 5, 6, 7
+
+if((currentLocation == 'A' || currentLocation == 'B') && (targetLocation == 'A' || targetLocation == 'B'))
+{
+  int temp = (targetIndex - currentIndex) * 50;
+  if (temp < 0) 
+  {
+    int temp2 = temp + 200;
+    numOfPulses = temp2 * 3;
+  } else 
+  {
+    numOfPulses = temp * 3;
+  }
+
+
+  if(targetLocation == 'A') 
+  {
+    //First Set of LEDS light up
+    for(int LEDIt = 0; LEDIt<20; LEDIt++)
+    {
+      if(LEDIt<7)
+      {
+        LedStrip[LEDIt] = 0x009999;
+      }
+      else
+      {
+        LedStrip[LEDIt]= CRGB::Black;
+      }   
+    }
+    FastLED.show();    
+    delay(10);  
+  }
+  else if(targetLocation == 'B') 
+  {
+    //First Set of LEDS light up
+    for(int LEDIt = 0; LEDIt<21; LEDIt++)
+    {
+      if(LEDIt>7 && LEDIt<14)
+      {
+        LedStrip[LEDIt] = 0x009999;
+      }
+      else
+      {
+        LedStrip[LEDIt]=CRGB::Black;
+      }
+    }
+    FastLED.show();    
+    delay(10);  
+  }
+
+  Serial.println("(Before) Location: " + String(currentLocation) + "" + String(currentIndex));
+  Serial.println("(After) Location: " + input);
+  Serial.println("Steps taken: " + String(numOfPulses));
+  Serial.println();
+}
+else if ((currentLocation == 'A' || currentLocation == 'B') && (targetLocation == 'C')) {
+  int positionC = targetIndex / 2;
+  int temp = (positionC - currentIndex) * 50;
+  if (temp < 0) 
+  {
+    int temp2 = temp + 200;
+    numOfPulses = temp2 * 3;
+  } 
+  else 
+  {
+    numOfPulses = temp * 3;
+  }
+
+  if(targetLocation == 'C') 
+  {
+    //First Set of LEDS light up    
+    for(int LEDIt = 0; LEDIt<21; LEDIt++)
+    {
+      if(LEDIt>14)
+      {
+        LedStrip[LEDIt] = 0x009999;
+      }
+      else
+      {
+        LedStrip[LEDIt]=CRGB::Black;
+      }
+    }
+  
+    FastLED.show();     
+    delay(10);
+  }
+  Serial.println("(Before) Location: " + String(currentLocation) + "" + String(currentIndex));
+  Serial.println("(After) Location: " + input);
+  Serial.println("Steps taken: " + String(numOfPulses));
+  Serial.println();
+}
+else if ((currentLocation == 'C') && (targetLocation == 'A' || targetLocation == 'B')) {
+  int positionC = currentIndex / 2;
+  int temp = (targetIndex - positionC) * 50;
+  if (temp < 0) 
+  {
+    int temp2 = temp + 200;
+    numOfPulses = temp2 * 3;
+  } 
+  else 
+  {
+    numOfPulses = temp * 3;
+  }
+
+  if(targetLocation == 'A') 
+  {
+    //First Set of LEDS light up
+    for(int LEDIt = 0; LEDIt<21; LEDIt++)
+    {
+      if(LEDIt<=7)
+      {
+        LedStrip[LEDIt] = 0x009999;
+      }
+      else
+      {
+        LedStrip[LEDIt]=CRGB::Black;
+      }   
+    }
+    FastLED.show();    
+    delay(10);  
+  }
+  else if(targetLocation == 'B') 
+  {
+    //First Set of LEDS light up
+    for(int LEDIt = 0; LEDIt<21; LEDIt++)
+    {
+      if(LEDIt>7 && LEDIt<14)
+      {
+        LedStrip[LEDIt] = 0x009999;
+      }
+      else
+      {
+        LedStrip[LEDIt]=CRGB::Black;
+      }
+    }
+    FastLED.show();    
+    delay(10);  
+  }
+       
+  Serial.println("(Before) Location: " + String(currentLocation) + "" + String(currentIndex));
+  Serial.println("(After) Location: " + input);
+  Serial.println("Steps taken: " + String(numOfPulses));
+  Serial.println();
+}
+else if (currentLocation == 'C' && targetLocation == 'C') 
+{
+  int positionC = targetIndex / 2;
+  currentIndex = currentIndex / 2;
+  int temp = (positionC - currentIndex) * 50;
+
+  if (temp < 0) 
+  {
+    int temp2 = temp + 200;
+    numOfPulses = temp2 * 3;
+  } else 
+  {
+    numOfPulses = temp * 3;
+  }
+  if(targetLocation == 'C') 
+  {
+    //First Set of LEDS light up    
+    for(int LEDIt = 0; LEDIt<21; LEDIt++)
+    {
+      if(LEDIt>14)
+      {
+        LedStrip[LEDIt]= 0x009999;
+      }
+      else
+      {
+        LedStrip[LEDIt]=CRGB::Black;
+      }
+    }
+    FastLED.show();     
+    delay(10);
+  }
+  Serial.println("(Before) Location: " + String(currentLocation) + "" + String(currentIndex));
+  Serial.println("(After) Location: " + input);
+  Serial.println("Steps taken: " + String(numOfPulses));
+  Serial.println();
+
+}
+
+//Signal the motor now
+digitalWrite(dirPin, HIGH);
+for (int y = 0; y < (numOfPulses); y++) 
+{
+  digitalWrite(stepPin, HIGH);
+  delayMicroseconds(3000);
+  digitalWrite(stepPin, LOW);
+  delayMicroseconds(3000);
+}
+numOfPulses = 0;
+delay(1000);                       //One second delay
+currentLocation = targetLocation;  //Current position is now the old target position
+  currentIndex = targetIndex;
   }
 }
